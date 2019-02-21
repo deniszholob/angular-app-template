41711b8e412146df1f36b01a04c8f5d1a3906ea2
Denis Zholob <dddgamer3x@gmail.com>
2019-02-21 14:31:59 -0700

    Test manifest scope and url

--- build-scripts/update-date.sh
+++ build-scripts/update-date.sh
@@ -12,4 +12,5 @@ file="src/app/app-update.ts"
 file_comment="// Auto Generated during deployment build //\n"
 file_content="export const APP_UPDATE_DATE = '%s';\n"
 printf "$file_comment$file_content" "$now" > $file
+echo "$now"
 echo 'Update Date script finished...'
